4db17bc07a326acfe1b23a80e47afc13a8265c96
Stepan Jamalayn <stepan.jamalyan@gmail.com>
2021-03-24 20:33:32 +0600

    built the netflix movie rows

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './App.css'
+import Row from './Row'
+import requests from './requests'
+
+function App() {
+   return (
+      <div className="App">
+         <h1>NETFLIX</h1>
+         <Row
+            title="NETFLIX ORIGINALS"
+            fetchUrl={requests.fetchNetflixOriginals}
+            isLargeRow
+         />
+         <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+         <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+         <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+         <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+         <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+         <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+         <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      </div>
+   )
+}
+
+export default App
+
+// https://api.themoviedb.org/3/movie/550?api_key=
